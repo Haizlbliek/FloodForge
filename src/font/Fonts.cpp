d8050e85daa7b907fc3ee680446d8cf4bdd3d9d6
Haizlbliek <haizlbliek@gmail.com>
2025-12-28 14:23:31 -0500

    Remove unused Ancient font

--- src/font/Fonts.cpp
+++ src/font/Fonts.cpp
@@ -2,14 +2,12 @@
 
 namespace Fonts {
 	Font *rainworld;
-	Font *ancient;
 	Font *rodondo;
 }
 
 void Fonts::init() {
 	Logger::info("Loading fonts...");
 	Fonts::rainworld = new Font("rainworld", FONT_SHARP);
-	Fonts::ancient = new Font("AncientBraidsExtended", FONT_SMOOTH);
 	Fonts::rodondo = new Font("Rodondo", FONT_SMOOTH);
 	Logger::info("Fonts loaded");
 	Logger::info();
@@ -17,6 +15,5 @@ void Fonts::init() {
 
 void Fonts::cleanup() {
 	delete Fonts::rainworld;
-	delete Fonts::ancient;
 	delete Fonts::rodondo;
 }
