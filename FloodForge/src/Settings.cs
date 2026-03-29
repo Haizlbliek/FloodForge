41859e71f2532e9d03a2ae3af2271e8944bac055
Haizlbliek <linus.denning@gmail.com>
2026-03-29 16:35:10 -0400

    Add rounded UI setting

--- FloodForge/src/Settings.cs
+++ FloodForge/src/Settings.cs
@@ -26,6 +26,7 @@ public static class Settings {
 	public static SubregionColorsSetting SubregionColors = new SubregionColorsSetting("SubregionColors", [ Color.Red, Color.Green, Color.Blue, Color.Yellow, Color.Cyan, Color.Magenta, new Color(1f, 0.5f, 0f), new Color(0.5f, 0.5f, 0.5f), new Color(0.5f, 0f, 1f), new Color(1f, 0.5f, 1f) ]);
 	public static Setting<bool> DisableAprilFoolsUpdates = Setting.Of("DisableAprilFoolsUpdates", false);
 	public static Setting<bool> DiscordRichPresence = Setting.Of("DiscordRichPresence", true);
+	public static Setting<bool> RoundedUI = Setting.Of("RoundedUI", false);
 
 	public static Setting<bool> DEBUGVisibleOutputPadding = Setting.Of("DebugVisibleOutputPadding", false);
 	public static Setting<bool> DEBUGVisiblePopupVisuals = Setting.Of("DebugVisiblePopupVisuals", false);
