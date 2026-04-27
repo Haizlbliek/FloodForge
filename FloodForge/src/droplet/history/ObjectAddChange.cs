5281c314a8b79759fc3d7026fdd94106c592b396
KhymeraSky <khy.skybreacher@gmail.com>
2026-04-27 16:22:07 +0200

    Add ObjectAddChange class

---
+++ FloodForge/src/droplet/history/ObjectAddChange.cs
@@ -0,0 +1,36 @@
+using FloodForge.World;
+
+namespace FloodForge.History;
+
+public class ObjectAddChange : Change {
+    bool adding;
+    DevObject objectToAdd;
+    Room room;
+    public ObjectAddChange(Room room, DevObject devObject, bool adding) {
+        this.objectToAdd = devObject;
+        this.adding = adding;
+        this.room = room;
+    }
+
+    private void Add() {
+        this.room.data.objects.Add(this.objectToAdd);
+    }
+
+    private void Remove() {
+        this.room.data.objects.Remove(this.objectToAdd);
+    }
+
+	public override void Redo() {
+        if (this.adding)
+            this.Add();
+        else
+            this.Remove();
+    }
+
+	public override void Undo() {
+        if (this.adding)
+            this.Remove();
+        else
+            this.Add();
+	}
+}
