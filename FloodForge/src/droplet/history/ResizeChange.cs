12b843fd2f34ba9336d0b1ab7f3f53b59ee529b0
KhymeraSky <khy.skybreacher@gmail.com>
2026-04-27 15:30:20 +0200

    Add ResizeChange

---
+++ FloodForge/src/droplet/history/ResizeChange.cs
@@ -0,0 +1,28 @@
+using FloodForge.Droplet;
+
+namespace FloodForge.History;
+
+public class ResizeChange : Change {
+    uint[] oldGeo;
+    uint[] newGeo;
+    Vector2i oldSize;
+    Vector2i newSize;
+
+    public ResizeChange(uint[] oldGeo, uint[] newGeo, Vector2i oldSize, Vector2i newSize) {
+        this.oldGeo = oldGeo;
+        this.newGeo = newGeo;
+        this.oldSize = oldSize;
+        this.newSize = newSize;
+    }
+    public override void Redo() {
+        DropletWindow.Room.width = this.newSize.x;
+        DropletWindow.Room.height = this.newSize.y;
+        DropletWindow.Room.geometry = this.newGeo;
+    }
+
+	public override void Undo() {
+        DropletWindow.Room.width = this.oldSize.x;
+        DropletWindow.Room.height = this.oldSize.y;
+        DropletWindow.Room.geometry = this.oldGeo;
+	}
+}
