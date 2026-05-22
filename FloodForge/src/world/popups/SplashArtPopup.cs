56495021b7b57fd6ade2e6445b66784cddd72154
Haizlbliek <linus.denning@gmail.com>
2026-05-22 12:44:24 -0400

    Attempt nightly to release switcher

--- FloodForge/src/world/popups/SplashArtPopup.cs
+++ FloodForge/src/world/popups/SplashArtPopup.cs
@@ -1,6 +1,7 @@
 using System.Diagnostics;
 using System.Net.Http;
 using System.Text.Json.Nodes;
+using System.Threading.Tasks;
 using FloodForge.Popups;
 using Stride.Core.Extensions;
 
@@ -56,111 +57,133 @@ public class SplashArtPopup : Popup {
 
 	private class FailedException : Exception {}
 
+	private static readonly HttpClient client = new HttpClient(new HttpClientHandler()) {
+		DefaultRequestHeaders = { { "User-Agent", "FloodForge-Updater" } }
+	};
+
 	private async void CheckForUpdates() {
 		try {
-			HttpClient client = new HttpClient();
-			client.DefaultRequestHeaders.Add("User-Agent", "FloodForge-Updater");
-			string url = this.nightlyBuildDate == null ? "https://api.github.com/repos/Haizlbliek/FloodForge/releases/latest" : "https://api.github.com/repos/Haizlbliek/FloodForge/releases/tags/nightly";
-			Stream response = await client.GetStreamAsync(url);
-			JsonNode? node = await JsonNode.ParseAsync(response);
-			if (node == null) {
-				Logger.Warn("Failed to fetch release version");
-				this.updateStatus = UpdateStatus.Failed;
-				throw new FailedException();
-			}
+			string currentUrl = this.nightlyBuildDate == null
+				? "https://api.github.com/repos/Haizlbliek/FloodForge/releases/latest"
+				: "https://api.github.com/repos/Haizlbliek/FloodForge/releases/tags/nightly";
+
+			Stream currentResponse = await client.GetStreamAsync(currentUrl);
+			JsonNode? currentNode = await JsonNode.ParseAsync(currentResponse) ?? throw new FailedException();
+
+			bool currentAvailable = false;
 			if (this.nightlyBuildDate == null) {
-				JsonNode? node2 = node["tag_name"];
-				if (node2 == null) {
-					Logger.Warn("Failed to fetch release version");
-					this.updateStatus = UpdateStatus.Failed;
-					throw new FailedException();
-				}
-				string? value = (string?) node2;
-				if (value == null) {
-					Logger.Warn("Failed to fetch release version");
-					this.updateStatus = UpdateStatus.Failed;
-					throw new FailedException();
-				}
+				string? value = (string?)currentNode["tag_name"] ?? throw new FailedException();
 				AppVersion latest = new AppVersion(value);
-				this.updateStatus = (this.version < latest) ? UpdateStatus.Available : UpdateStatus.Unavailable;
+				currentAvailable = this.version < latest;
 			}
 			else {
-				string? body = node["body"]?.ToString().Replace("BUILD_DATE: ", "").Trim();
-				if (body == null || !DateTime.TryParse(body, out DateTime date)) {
-					Logger.Warn("Failed to fetch release body");
-					this.updateStatus = UpdateStatus.Failed;
+				string? body = currentNode["body"]?.ToString().Replace("BUILD_DATE: ", "").Trim();
+				if (body == null || !DateTime.TryParse(body, out DateTime date))
 					throw new FailedException();
-				}
 
-				this.updateStatus = (this.nightlyBuildDate < date) ? UpdateStatus.Available : UpdateStatus.Unavailable;
+				currentAvailable = this.nightlyBuildDate < date;
 			}
+
+			this.updateStatus = currentAvailable ? UpdateStatus.Available : UpdateStatus.Unavailable;
+
+			string switchText = this.nightlyBuildDate == null ? "Switch to Nightly" : "Switch to Release";
+			this.buttons.Add(new IconButton(switchText, 0.25f, 0.25f, 0.5f, 0.5f, () => {
+				PopupManager.Add(new ConfirmPopup($"Switch to the latest {(this.nightlyBuildDate == null ? "Nightly" : "Release")} build?").Okay(async () => {
+					PopupManager.Add(new InfoPopup("Fetching alternate branch info..."));
+					await this.FetchAndDownloadTrack(this.nightlyBuildDate == null);
+				}));
+				return true;
+			}));
 
 			if (this.updateStatus == UpdateStatus.Available) {
-				string osPart = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "Windows" : "Linux";
-				string archPart = "";
-				if (RuntimeInformation.ProcessArchitecture == Architecture.Arm64) {
-					archPart = "-arm64";
-				}
-				string targetFileName = $"FloodForge-{osPart}{archPart}.zip";
-				JsonArray? assets = node["assets"]?.AsArray();
-				string? downloadUrl = null;
-				if (assets != null) {
-					foreach (JsonNode? asset in assets) {
-						if (asset?["name"]?.ToString().Equals(targetFileName, StringComparison.InvariantCultureIgnoreCase) ?? false) {
-							downloadUrl = asset["browser_download_url"]?.ToString();
-							string? digest = asset["digest"]?.ToString();
-							this.updateChecksum = digest?.Replace("sha256:", "");
-							break;
-						}
-					}
-				}
-
-				if (this.updateChecksum == null) {
-					Logger.Warn("Failed to fetch latest release checksum");
-					this.updateStatus = UpdateStatus.Failed;
+				(string? downloadUrl, string? checksum) = this.ParseAssetData(currentNode);
+				if (downloadUrl == null || checksum == null)
 					throw new FailedException();
-				}
 
-				if (!string.IsNullOrEmpty(downloadUrl)) {
-					this.updatePath = downloadUrl;
+				this.updatePath = downloadUrl;
+				this.updateChecksum = checksum;
 
-					this.buttons.Add(new IconButton("Update Available!", 0.5f, 0f, 0.75f, 0.25f, () => {
-						PopupManager.Add(new ConfirmPopup($"Update to latest {(this.nightlyBuildDate == null ? "version" : "nightly")}?").Okay(async () => {
-							PopupManager.Add(new InfoPopup("Downloading, please wait."));
-							Logger.Note($"Downloading {this.updatePath} {this.updateChecksum}");
-							await Updater.Download(this.updatePath, this.updateChecksum!);
-							Logger.Note($"Downloaded");
-						}));
-						return true;
+				this.buttons.Add(new IconButton("Update Available!", 0.5f, 0f, 0.75f, 0.25f, () => {
+					PopupManager.Add(new ConfirmPopup($"Update to latest {(this.nightlyBuildDate == null ? "version" : "nightly")}?").Okay(async () => {
+						PopupManager.Add(new InfoPopup("Downloading, please wait."));
+						Logger.Note($"Downloading {this.updatePath} {this.updateChecksum}");
+						await Updater.Download(this.updatePath, this.updateChecksum!);
+						Logger.Note($"Downloaded");
 					}));
+					return true;
+				}));
+			}
+		}
+		catch (Exception ex) when (ex is HttpRequestException or FailedException) {
+			this.HandleUpdateFailure(ex is not FailedException ? ex : null);
+		}
+	}
+
+	private async Task FetchAndDownloadTrack(bool targetIsNightly) {
+		try {
+			string targetUrl = targetIsNightly
+				? "https://api.github.com/repos/Haizlbliek/FloodForge/releases/tags/nightly"
+				: "https://api.github.com/repos/Haizlbliek/FloodForge/releases/latest";
+
+			Stream response = await client.GetStreamAsync(targetUrl);
+			JsonNode? node = await JsonNode.ParseAsync(response) ?? throw new FailedException();
+
+			(string? downloadUrl, string? checksum) = this.ParseAssetData(node);
+			if (downloadUrl == null || checksum == null)
+				throw new FailedException();
+
+			if (!targetIsNightly) {
+				string nightlyFilePath = Path.Combine("assets", "nightly.txt");
+				if (File.Exists(nightlyFilePath)) {
+					File.Delete(nightlyFilePath);
 				}
-				else {
-					Logger.Warn($"Could not find a release asset for {targetFileName}");
+			}
+
+			PopupManager.Add(new InfoPopup($"Downloading target branch, please wait..."));
+			Logger.Note($"Switching track: Downloading {downloadUrl} {checksum}");
+			await Updater.Download(downloadUrl, checksum);
+			Logger.Note($"Downloaded successfully");
+		}
+		catch (Exception ex) {
+			Logger.Warn("Failed to switch tracks");
+			Logger.Info(ex);
+			PopupManager.Add(new InfoPopup("Failed to download the alternative build."));
+		}
+	}
+
+	private void HandleUpdateFailure(Exception? ex) {
+		Logger.Warn("Failed to fetch update information");
+		if (ex != null) Logger.Info(ex);
+
+		this.updateStatus = UpdateStatus.Failed;
+
+		IconButton b = null!;
+		b = new IconButton("Failed to fetch Update", 0.75f, 0f, 1f, 0.25f, () => {
+			this.updateStatus = UpdateStatus.Searching;
+			this.CheckForUpdates();
+			return false;
+		});
+		this.buttons.Add(b);
+	}
+
+	private (string? downloadUrl, string? checksum) ParseAssetData(JsonNode node) {
+		string osPart = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "Windows" : "Linux";
+		string archPart = RuntimeInformation.ProcessArchitecture == Architecture.Arm64 ? "-arm64" : "";
+		string targetFileName = $"FloodForge-{osPart}{archPart}.zip";
+
+		JsonArray? assets = node["assets"]?.AsArray();
+		if (assets != null) {
+			foreach (JsonNode? asset in assets) {
+				if (asset?["name"]?.ToString().Equals(targetFileName, StringComparison.InvariantCultureIgnoreCase) ?? false) {
+					string? downloadUrl = asset["browser_download_url"]?.ToString();
+					string? digest = asset["digest"]?.ToString();
+					string? checksum = digest?.Replace("sha256:", "");
+					return (downloadUrl, checksum);
 				}
 			}
 		}
-		catch (HttpRequestException) {
-			Logger.Warn("Failed to fetch latest release version");
-			this.updateStatus = UpdateStatus.Failed;
-		}
-		catch (FailedException) {
-		}
-		catch (Exception ex) {
-			Logger.Warn("Failed to fetch latest release version");
-			Logger.Info(ex);
-			this.updateStatus = UpdateStatus.Failed;
-		}
-
-		if (this.updateStatus == UpdateStatus.Failed) {
-			IconButton b = null!;
-			b = new IconButton("Failed to fetch Update", 0.75f, 0f, 1f, 0.25f, () => {
-				this.updateStatus = UpdateStatus.Searching;
-				this.CheckForUpdates();
-				this.buttons.Remove(b);
-				return false;
-			});
-			this.buttons.Add(b);
-		}
+		Logger.Warn($"Could not find a release asset for {targetFileName}");
+		return (null, null);
 	}
 
 	public override void Close() {
@@ -192,12 +215,13 @@ public class SplashArtPopup : Popup {
 		UI.rodondo.Write(Main.AprilFools ? "FlodoFroge" : "FloodForge", 0f, 0.3f, 0.2f, Font.Align.MiddleCenter);
 		UI.font.Write(Main.AprilFools ? "Wordle Editor" : "World Editor", 0f, 0.1f, 0.1f, Font.Align.MiddleCenter);
 		UI.font.Write(this.version.ToString(), -0.88f, 0.63f, 0.04f);
-	
+
 		Immediate.Color(0.8f, 0.8f, 0.8f);
 		UI.font.Write("Recent worlds:", -0.88f, -0.28f, 0.03f, Font.Align.MiddleLeft);
 
 		for (int i = 0; i < 8; i++) {
-			if (i >= RecentFiles.recents.Count) break;
+			if (i >= RecentFiles.recents.Count)
+				break;
 
 			string recent = RecentFiles.recentNames[i];
 			string acronym = "";
@@ -256,9 +280,9 @@ public class SplashArtPopup : Popup {
 			Immediate.UseTexture(this.uiIcons);
 			Program.gl.Enable(EnableCap.Blend);
 			Immediate.Color(1f, 1f, 1f);
-	
+
 			UI.FillRect(UVRect.FromSize(0.31f, startY - yOffset, 0.05f, 0.05f).UV(button.UVs.u1, button.UVs.v1, button.UVs.u2, button.UVs.v2));
-	
+
 			Immediate.UseTexture(0);
 			Program.gl.Disable(EnableCap.Blend);
 
