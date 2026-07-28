b8ed49effaf2f448c6ad82f43ae042eb56425942
KhymeraSky <khy.skybreacher@gmail.com>
2026-07-28 14:44:06 +0200

    Add Edit ReplaceRooms button to RoomSettingsPopup Add EditReplaceRoomPopup

--- FloodForge/src/world/popups/ModularPopups/RoomSettingsPopup.cs
+++ FloodForge/src/world/popups/ModularPopups/RoomSettingsPopup.cs
@@ -3,6 +3,7 @@ using FloodForge.History;
 using FloodForge.Droplet;
 using StbImageWriteSharp;
 using static FloodForge.Main;
+using System.Text.RegularExpressions;
 
 namespace FloodForge.World;
 
@@ -15,6 +16,9 @@ public class RoomSettingsPopup : ModularPopup {
 	private ButtonContainer renameRoomButton;
 	private ButtonContainer createTimelineRoomButton;
 	private CreateTimelineRoomPopup? createTimelineRoomPopup;
+
+	private ButtonContainer editReplaceRooms;
+	private EditReplaceRoomPopup? editReplaceRoomPopup;
 
 	public RoomSettingsPopup(Room relevantRoom) {
 		this.relevantRoom = relevantRoom;
@@ -33,6 +37,8 @@ public class RoomSettingsPopup : ModularPopup {
 		this.AddToQueue(this.renameRoomButton);
 		this.createTimelineRoomButton = new ButtonContainer("Create Timeline Room", this.AddCreateTimelineRoomPopup);
 		this.AddToQueue(this.createTimelineRoomButton);
+		this.editReplaceRooms = new ButtonContainer("Edit ReplaceRooms", this.EditReplaceRooms);
+		this.AddToQueue(this.editReplaceRooms);
 		this.AddQueuedSettings();
 	}
 
@@ -81,6 +87,218 @@ public class RoomSettingsPopup : ModularPopup {
 					PopupManager.Add(new InfoPopup($"Problem encountered."));
 				}
 			}).Translate(Mouse.Pos, true).Title("Rename Room"));
+		}
+	}
+
+	private void EditReplaceRooms() {
+		this.editReplaceRoomPopup = (EditReplaceRoomPopup)new EditReplaceRoomPopup(this.relevantRoom).Translate(Mouse.Pos, false);
+		PopupManager.Add(this.editReplaceRoomPopup);
+	}
+
+	public class EditReplaceRoomPopup : ModularPopup {
+		private Room relevantRoom;
+		private LabelContainer tutorialLabel;
+		private ButtonContainer expandMenuButton;
+		bool menuExpanded = false;
+
+		private List<(ReplaceRoom replaceRoom, VerticalElement element)> associatedVerticalElements = [];
+
+		private Timeline replaceRoomTimeline;
+		private bool TimelineSelected => this.replaceRoomTimeline.timelines.Count != 0;
+
+		private ButtonContainer timelineButton;
+		private enum MenuMode {
+			closed,
+			createNew,
+			fromRoom,
+			fromFile
+		}
+		private MenuMode menuMode;
+		private ButtonContainer createNewModeButton, fromRoomModeButton, fromFileModeButton;
+		private HorizontalElement menuModeButtons;
+
+		// fromFile
+		private string filePath;
+		private ButtonContainer openFilePopupButton;
+		private LabelContainer filePathLabel;
+		private ButtonContainer createFromFileButton;
+
+		public EditReplaceRoomPopup(Room room) {
+			this.relevantRoom = room;
+			this.popupTitle = $"{room.name} - Edit Replacerooms";
+			this.tutorialLabel = new LabelContainer("ReplaceRooms apply from top to bottom.\nThe last item in the list has priority.");
+			this.expandMenuButton = new ButtonContainer("Add ReplaceRoom", () => { this.menuExpanded = !this.menuExpanded; if(this.menuExpanded) this.ResetReplaceRoomParameters(); }).SetContextCheck(b => { b.settingName = this.menuExpanded ? "Cancel" : "Add ReplaceRoom"; return true; });
+			this.replaceRoomTimeline = new(TimelineType.Only, []);
+
+			this.createNewModeButton = new ButtonContainer("create New", () => this.menuMode = MenuMode.createNew).SetContextCheck(_ => this.menuMode == MenuMode.createNew, true, false);
+			this.fromRoomModeButton = new ButtonContainer("from Room", () => this.menuMode = MenuMode.fromRoom).SetContextCheck(_ => this.menuMode == MenuMode.fromRoom, true, false);
+			this.fromFileModeButton = new ButtonContainer("from File", () => this.menuMode = MenuMode.fromFile).SetContextCheck(_ => this.menuMode == MenuMode.fromFile, true, false);
+			this.menuModeButtons = new HorizontalElement([("cn", this.createNewModeButton), ("fr", this.fromRoomModeButton), ("ff", this.fromFileModeButton)], null, false, true);
+			
+			this.timelineButton = new ButtonContainer("Timeline", this.OpenTimelinePopup).SetContextCheck(b => {
+				b.settingName = "Timeline" + (this.replaceRoomTimeline.timelines.Count == 0 ? "" : $" - {this.replaceRoomTimeline}"); this.RecalculateBounds(true, true); return true;});
+			
+			this.openFilePopupButton = new ButtonContainer("Select File", this.OpenFileSystem);
+			this.filePath = "";
+			this.filePathLabel = new LabelContainer("None selected", autoCrop: true, fromRight: true);
+			this.createFromFileButton = new ButtonContainer("Create from File", this.CreateFromFile).SetContextCheck(_ => File.Exists(this.filePath) && this.TimelineSelected, true);
+
+			this.RebuildSettings(false);
+		}
+
+		public override void Draw() {
+			Immediate.Color(Color.Yellow);
+			UI.Line(new(this.bounds.x0, this.bounds.y1), (this.relevantRoom.Position - WorldWindow.cameraOffset) / WorldWindow.cameraScale);
+			if (this.isHovered)
+				this.RebuildSettings();
+			base.Draw();
+		}
+
+		private void RebuildSettings(bool setPos = true) {
+			this.settingContainers = [];
+			this.associatedVerticalElements = [];
+			this.AddToQueue(this.tutorialLabel);
+			this.AddToQueue(new Divider());
+			foreach (ReplaceRoom replacingRoom in this.relevantRoom.replaceRooms) {
+				LabelContainer replaceRoomLabel = new LabelContainer("");
+				ButtonContainer viewButton = new ButtonContainer("View", () => this.ViewReplaceRoom(replacingRoom));
+				HorizontalElement reorderButtons = new ([("up", new ButtonContainer("/\\", () => this.Move(replacingRoom, true))), ("down", new ButtonContainer("\\/", () => this.Move(replacingRoom, false)))], null, false, true);
+				HorizontalElement buttonElement = new ([("view", viewButton), ("movebuttons", reorderButtons)], [0f, 0.12f]);
+				VerticalElement finalElement = new ([("label", replaceRoomLabel), ("buttons", buttonElement)]);
+				this.associatedVerticalElements.Add((replacingRoom, finalElement));
+				this.AddToQueue(finalElement);
+			}
+			this.AddToQueue(new Divider());
+			this.AddToQueue(this.expandMenuButton);
+			if (this.menuExpanded) {
+				this.AddToQueue(this.timelineButton);
+				if (this.TimelineSelected) {
+					this.AddToQueue(this.menuModeButtons);
+					this.AddToQueue(new Divider());
+					switch (this.menuMode) {
+						case MenuMode.createNew:
+						case MenuMode.fromRoom:
+							this.AddToQueue(new LabelContainer("Not implemented yet."));
+							this.AddToQueue(new LabelContainer("Not implemented yet."));
+						break;
+						case MenuMode.fromFile:
+							this.AddToQueue(this.openFilePopupButton);
+							this.filePathLabel.settingName = this.filePath == "" ? "None selected" : this.filePath;
+							this.AddToQueue(this.filePathLabel);
+							this.AddToQueue(this.createFromFileButton);
+						break;
+					}
+				}
+			}
+			this.UpdateReplaceRoomLabels();
+			this.AddQueuedSettings(setPos);
+		}
+
+		private void ResetReplaceRoomParameters() {
+			this.replaceRoomTimeline = new(TimelineType.Only, []);
+			this.filePath = "";
+			this.filePathLabel.settingName = "None selected";
+			this.menuMode = MenuMode.closed;
+		}
+
+		private void OpenTimelinePopup() {
+			PopupManager.Add(new TimelinePopup(this.replaceRoomTimeline, _ => {},
+			(enabled, timeline) => {
+				if (!enabled)
+					this.replaceRoomTimeline.timelines.Add(timeline);
+				else
+					this.replaceRoomTimeline.timelines.Remove(timeline);
+			}, true).SetButtons<TimelinePopup>("", "REPLACE", "").Translate(Mouse.Pos, false).Title("Specify ReplaceRoom Timeline"), true);
+		}
+
+		private void OpenFileSystem() {
+			PopupManager.Add(new FilesystemPopup(this.SelectFile, 1).Hint("xx_a01_future.txt").Filter(new Regex("((?!.*_settings)(?=.+_.+).+\\.txt)|(gate_([^._-]+)_([^._-]+)\\.txt)")).Title("Select room file to use"), true);
+		}
+
+		private void SelectFile(string[] selectedFileArray) {
+			if (selectedFileArray.Length == 0)
+				return;
+			this.filePath = selectedFileArray.First();
+			this.filePathLabel.settingName = UI.font.CropText(this.filePath, this.usableBounds.x1 - this.usableBounds.x0, 0.03f, out _, true);
+			this.RecalculateBounds(true, true);
+		}
+
+		private void CreateFromFile() {
+			string newRoomName = Path.GetFileNameWithoutExtension(this.filePath);
+			
+			Room? existingRoom = null;
+			foreach (Room room in WorldWindow.region.rooms) {
+				if (room.name == newRoomName) {
+					existingRoom = room;
+					break;
+				}
+			}
+			if (existingRoom != null) {
+				PopupManager.Add(new InfoPopup($"A room with the name\n'{newRoomName}'\nalready exists\ncreate from room instead"));
+				return;
+			}
+			
+			string key = "CreateReplaceRoomFromFile";
+			WorldWindow.worldHistory.StartCollectingChanges([typeof(RoomAndConnectionChange)], key);
+			Room addedRoom = WorldWindow.HandleRoomFilesSelected([this.filePath]).First();
+			WorldWindow.worldHistory.StopCollectingChanges(key);
+
+			addedRoom.isVirtualRoom = true;
+			ReplaceRoom newReplaceRoom = new(addedRoom, this.relevantRoom, this.replaceRoomTimeline, []);
+			ReplaceRoomChange replaceRoomChange = new(true);
+			replaceRoomChange.AddReplaceRoom(newReplaceRoom);
+			WorldWindow.worldHistory.Apply(replaceRoomChange);
+			this.Close();
+
+			if (newReplaceRoom.replacedRoom.roomExits.Count != newReplaceRoom.replacingRoom.roomExits.Count)
+				PopupManager.Add($"New ReplaceRoom {newReplaceRoom.replacingRoom.name}\nhas different exit count from {newReplaceRoom.replacedRoom.name}\n(this may cause issues)");
+		}
+
+		private void Move(ReplaceRoom replaceRoomToMove, bool up) {
+			int direction = up ? -1 : 1;
+
+			int originalRoomIndex = 0;
+			int newRoomIndex = 0;
+			ReplaceRoom? replaceRoomToSwapWith = null;
+
+			for (; originalRoomIndex < this.relevantRoom.replaceRooms.Count; originalRoomIndex++) {
+				if (this.relevantRoom.replaceRooms[originalRoomIndex] == replaceRoomToMove) {
+					newRoomIndex = Math.Clamp(newRoomIndex + direction, 0, this.relevantRoom.replaceRooms.Count - 1);
+					replaceRoomToSwapWith = this.relevantRoom.replaceRooms[newRoomIndex];
+					break;
+				}
+			}
+			if (replaceRoomToSwapWith == null || replaceRoomToSwapWith == replaceRoomToMove)
+				return;
+			
+			int originalWorldIndex = 0;
+			int newWorldIndex = 0;
+			for (int i = 0; i < WorldWindow.replaceRooms.Count; i++) {
+				if (WorldWindow.replaceRooms[i] == replaceRoomToMove)
+					originalWorldIndex = i;
+				if (WorldWindow.replaceRooms[i] == replaceRoomToSwapWith)
+					newWorldIndex = i;
+			}
+			
+			// swap the relevant items
+			WorldWindow.worldHistory.Apply(new MassChange([new ListSwapChange<ReplaceRoom>(this.relevantRoom.replaceRooms, originalRoomIndex, newRoomIndex), new ListSwapChange<ReplaceRoom>(WorldWindow.replaceRooms, originalWorldIndex, newWorldIndex)], () => this.RebuildSettings()));
+		}
+
+		private void ViewReplaceRoom(ReplaceRoom replaceRoomToView) {
+			PopupManager.Add(new ReplaceRoomSettingsPopup(replaceRoomToView));
+		}
+
+		private void UpdateReplaceRoomLabels() {
+			foreach ((ReplaceRoom replaceroom, VerticalElement verticalElement) in this.associatedVerticalElements) {
+				string preProcessorConditionsToString = "";
+				bool first = true;
+				foreach (string condition in replaceroom.preProcessorConditions) {
+					preProcessorConditionsToString += (first ? "" : ",") + condition;
+				}
+				preProcessorConditionsToString = preProcessorConditionsToString == "" ? "" :  $"{{{preProcessorConditionsToString}}}";
+				verticalElement.GetByID("label")?.settingName = $"{preProcessorConditionsToString}({replaceroom.timeline}) -> {replaceroom.replacingRoom.name}";
+			}
+			this.RecalculateBounds(true, true);
 		}
 	}
 
